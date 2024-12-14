c17532c5d156cd7751ff6e8ca4fbfd8032b82e06
sweetsoop <sweetsoop@gmail.com>
2024-12-14 18:12:19 +0900

    log 메소드 변경

--- lib/logger/logger.dart
+++ lib/logger/logger.dart
@@ -7,8 +7,8 @@ mixin LogService {
 
   void setName(String name) => _name = name.padRight(_nameSize);
 
-  void d(String message) => log('\x1B[32m[$_name] $message\x1B[0m', name: 'SS_DEBUG ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
-  void i(String message) => log('\x1B[37m[$_name] $message\x1B[0m', name: 'SS_INFO  ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
-  void w(String message) => log('\x1B[33m[$_name] $message\x1B[0m', name: 'SS_WARN  ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
-  void e(String message) => log('\x1B[31m[$_name] $message\x1B[0m', name: 'SS_ERROR ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
+  void debug(String message) => log('\x1B[32m[$_name] $message\x1B[0m', name: 'SS_DEBUG ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
+  void info(String message) => log('\x1B[37m[$_name] $message\x1B[0m', name: 'SS_INFO  ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
+  void warn(String message) => log('\x1B[33m[$_name] $message\x1B[0m', name: 'SS_WARN  ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
+  void error(String message) => log('\x1B[31m[$_name] $message\x1B[0m', name: 'SS_ERROR ${DateFormat('y-MM-dd HH:mm:ss.SSS').format(DateTime.now())}');
 }
