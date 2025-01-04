9005bb6c685fd2f0b7bfbe13c2ac132bfd45b256
sweetsoop <sweetsoop@gmail.com>
2025-01-04 20:31:38 +0900

    durationToString

--- lib/datetime/datetime_util.dart
+++ lib/datetime/datetime_util.dart
@@ -4,4 +4,6 @@ class DateTimeUtil {
     final remainSeconds = seconds % 60;
     return (minute, remainSeconds);
   }
+
+  static String durationToString(Duration d) => d.toString().split('.').first.padLeft(8, '0');
 }
